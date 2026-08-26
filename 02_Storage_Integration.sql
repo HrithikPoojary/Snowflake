3a66cee918c95612fe76fa0380ae53d034782395
Hrithik Poojary <hrithikpoojary@Hrithiks-MacBook.local>
2026-08-26 22:22:38 +0530

    Storage integration

---
+++ 02_Storage_Integration.sql
@@ -0,0 +1,15 @@
+create or replace storage integration aws_s3_int
+type = external_stage
+storage_provider = s3
+enabled = TRUE 
+storage_aws_role_arn = 'arn:aws:iam::111035763792:role/SNOWFLAKE-S3-FULL-ACCESS'
+storage_allowed_locations = ('s3://snowflake-bucket-inv/')
+storage_blocked_locations = ('s3://snowflake-bucket-inv/secret_folder/')
+comment = 'Demo creating storage integration';
+
+
+alter storage integration aws_s3_int set enabled = FALSE;
+
+alter storage integration aws_s3_int set storage_allowed_locaions = ('path1' , 'path2');
+
+describe integration aws_s3_int;
