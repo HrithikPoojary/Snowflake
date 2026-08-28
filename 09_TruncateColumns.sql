df4bd011bfa7c434a8742ffa939b77ebc0e411fb
Hrithik Poojary <hrithikpoojary@Hrithiks-MacBook.local>
2026-08-28 23:19:43 +0530

    patterna and truncate column

---
+++ 09_TruncateColumns.sql
@@ -0,0 +1,25 @@
+use hrms.etl;
+
+create or replace table hrms.etl.employees_copy
+(
+employee_id number(6),
+first_name varchar(20),
+last_name varchar(20),
+email varchar(1),
+phone_number varchar(20),
+hire_dt date,
+job_id varchar(10),
+salary number(8,2),
+commission_pct decimal(2,2),
+manager_id number(10),
+department_id number(10)
+);
+
+copy into employees_copy
+from @aws_etl_csv_stage
+files = ('csv/employees_part_005.csv')
+truncatecolumns = true;
+
+list @aws_etl_csv_stage;
+
+select * from employees_copy;
