40794523a2d16cd5b9d3e98a3233c8302fe5193c
Hrithik Poojary <hrithikpoojary@Hrithiks-MacBook.local>
2026-08-29 00:41:35 +0530

    return only failed and size limit

---
+++ 12_Size_Limit.sql
@@ -0,0 +1,23 @@
+use hrms.etl;
+
+create or replace table hrms.etl.employees_copy
+(
+employee_id number(6),
+first_name varchar(20),
+last_name varchar(20),
+email varchar(20),
+phone_number varchar(20),
+hire_dt date,
+job_id varchar(10),
+salary number(8,2),
+commission_pct decimal(2,2),
+manager_id number(10),
+department_id number(10)
+);
+
+copy into employees_copy
+from @aws_etl_csv_stage
+on_error = continue 
+size_limit = 10000000;
+
+list @aws_etl_csv_stage;
